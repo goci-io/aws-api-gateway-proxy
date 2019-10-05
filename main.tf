0759cf9a7f10cbf14e3b6cbd121ccef3482df618
etwillbefine <tim.gatzemeier@gmx.de>
2019-10-06 00:09:10 +0200

    await deployment till method is created

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_api_gateway_resource" "proxy" {
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "request_method" {
+resource "aws_api_gateway_method" "proxy" {
   rest_api_id   = aws_api_gateway_rest_api.main.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
@@ -71,14 +71,15 @@ resource "aws_api_gateway_integration" "vpc" {
   rest_api_id          = aws_api_gateway_rest_api.main.id
   resource_id          = aws_api_gateway_resource.proxy.id
   connection_id        = aws_api_gateway_vpc_link.link.id
-  connection_type      = "VPC_LINK"
-  type                 = "HTTP_PROXY"
   uri                  = var.integration_uri
   http_method          = "ANY"
+  connection_type      = "VPC_LINK"
+  type                 = "HTTP_PROXY"
   passthrough_behavior = "WHEN_NO_MATCH"
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_method.proxy]
   rest_api_id = aws_api_gateway_rest_api.main.id
   stage_name  = var.stage
 }
