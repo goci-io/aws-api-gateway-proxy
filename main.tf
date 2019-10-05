2e523ef10c704de4b249176282cbde664d97187e
etwillbefine <tim.gatzemeier@gmx.de>
2019-10-06 00:14:59 +0200

    set missing integration_http_method, add cache and proxy params

--- main.tf
+++ main.tf
@@ -68,14 +68,16 @@ resource "aws_api_gateway_method" "proxy" {
 }
 
 resource "aws_api_gateway_integration" "vpc" {
-  rest_api_id          = aws_api_gateway_rest_api.main.id
-  resource_id          = aws_api_gateway_resource.proxy.id
-  connection_id        = aws_api_gateway_vpc_link.link.id
-  uri                  = var.integration_uri
-  http_method          = "ANY"
-  connection_type      = "VPC_LINK"
-  type                 = "HTTP_PROXY"
-  passthrough_behavior = "WHEN_NO_MATCH"
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  connection_id           = aws_api_gateway_vpc_link.link.id
+  uri                     = format("%s/{proxy}", var.integration_uri)
+  cache_key_parameters    = ["method.request.path.proxy"]
+  integration_http_method = "ANY"
+  http_method             = "ANY"
+  connection_type         = "VPC_LINK"
+  type                    = "HTTP_PROXY"
+  passthrough_behavior    = "WHEN_NO_MATCH"
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
