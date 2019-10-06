20023ebf38d481d99fc6d626d67c07b8e4b7207a
etwillbefine <tim.gatzemeier@gmx.de>
2019-10-06 17:52:24 +0200

    add domain.tf to create domain related resources

---
+++ domain.tf
@@ -0,0 +1,21 @@
+
+resource "aws_api_gateway_domain_name" "domain" {
+  domain_name              = local.domain_name
+  regional_certificate_arn = local.certificate_arn
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+module "apigw_record" {
+  source        = "git::https://github.com/goci-io/aws-route53-records.git?ref=master"
+  hosted_zone   = local.hosted_zone
+  alias_records = [
+    {
+      name       = var.name
+      alias      = aws_api_gateway_domain_name.domain.regional_domain_name
+      alias_zone = aws_api_gateway_domain_name.domain.regional_zone_id
+    }
+  ]
+}
