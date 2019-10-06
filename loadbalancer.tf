4d9402da9969fb28d6df6d9b68cd3e8252c3a8ba
etwillbefine <tim.gatzemeier@gmx.de>
2019-10-06 17:52:06 +0200

    use TCP protocol for listeners and target, configure https redirect

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -16,28 +16,45 @@ resource "aws_lb" "nlb" {
 }
 
 resource "aws_lb_target_group" "target" {
-  name     = module.label.id
-  tags     = module.label.tags
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = local.vpc_id
+  name                 = module.label.id
+  tags                 = module.label.tags
+  port                 = 80
+  protocol             = "TCP"
+  vpc_id               = local.vpc_id
+  deregistration_delay = 120
 
   health_check {
     healthy_threshold   = 2
     interval            = 30
     path                = var.health_endpoint
-    protocol            = "HTTP"
+    protocol            = "TCP"
   }
 }
 
 resource "aws_lb_listener" "forward" {
   load_balancer_arn = aws_lb.nlb.arn
   certificate_arn   = local.certificate_arn
-  protocol          = "TLS"
+  protocol          = "TCP"
   port              = 443
 
   default_action {
     target_group_arn = aws_lb_target_group.target.arn
     type             = "forward"
+  }
+}
+
+resource "aws_lb_listener" "https_redirect" {
+  load_balancer_arn = aws_lb.nlb.arn
+  protocol          = "HTTP"
+  port              = 80
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
