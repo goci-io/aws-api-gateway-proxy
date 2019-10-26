6dd3e11a414c2c7e6613e5b267f1393200bbf674
etwillbefine <tim.gatzemeier@gmx.de>
2019-10-26 23:32:00 +0200

    add base path mapping to custom domain

--- domain.tf
+++ domain.tf
@@ -9,6 +9,12 @@ resource "aws_api_gateway_domain_name" "domain" {
   }
 }
 
+resource "aws_api_gateway_base_path_mapping" "domain" {
+  api_id      = aws_api_gateway_rest_api.main.id
+  stage_name  = aws_api_gateway_deployment.deployment.stage_name
+  domain_name = aws_api_gateway_domain_name.domain.domain_name
+}
+
 module "apigw_record" {
   source      = "git::https://github.com/goci-io/aws-route53-records.git?ref=tags/0.2.0"
   hosted_zone = local.hosted_zone
