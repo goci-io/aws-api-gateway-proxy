8dd1918eccd99ff077dad7f960210269b4f80bdf
etwillbefine <tim.gatzemeier@gmx.de>
2019-10-27 18:20:22 +0100

    use wildcard method path for settings needed because proxy string or any is not valid in method_path of settings proxy results in all http method to be present in the stage and requires therefore wildcards for all methods

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_api_gateway_method" "proxy" {
 resource "aws_api_gateway_method_settings" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   stage_name  = aws_api_gateway_stage.stage.stage_name
-  method_path = "${aws_api_gateway_resource.proxy.path_part}/${aws_api_gateway_method.proxy.http_method}"
+  method_path = "*/*"
 
   settings {
     metrics_enabled = true
