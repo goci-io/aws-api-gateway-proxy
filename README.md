36ad96260bd5031e1e72c645448183174f5f5921
etwillbefine <tim.gatzemeier@gmx.de>
2019-11-01 17:52:55 +0100

    read hosted zone name from correct remote state

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# aws-api-gateway
+# aws-api-gateway-proxy
 
 **Maintained by [@goci-io/prp-terraform](https://github.com/orgs/goci-io/teams/prp-terraform)**
 
 This module creates a proxy integration with API Gateway to route traffic securely into a VPC and proxy an API within that VPC.
-The required Network Load Balancer can be placed into multiple subnets by providing the Subnet-ID or a reference to a remote state containing an output `private_subnet_ids`. 
+The required Network Load Balancer can be placed into multiple subnets by providing the Subnet-ID or a reference to a remote state containing an output `public_subnet_ids`. 
+
+*Note:* The Load Balancer should be placed into the public subnets of your VPC. (TBC with VPCE)
+
+### Usage
+
+
+### Configuration
--- remote.tf
+++ remote.tf
@@ -1,6 +1,6 @@
 locals {
   custom_cert     = var.use_letsencrypt_certificate ? module.letsencrypt.certificate_arn : var.certificate_arn
-  hosted_zone     = var.dns_module_state == "" ? var.domain_name : data.terraform_remote_state.acm[0].outputs.domain_name
+  hosted_zone     = var.dns_module_state == "" ? var.domain_name : data.terraform_remote_state.dns[0].outputs.domain_name
   certificate_arn = var.acm_module_state == "" ? local.custom_cert : data.terraform_remote_state.acm[0].outputs.certificate_arn
   vpc_id          = var.vpc_module_state == "" ? var.vpc_id : data.terraform_remote_state.vpc[0].outputs.vpc_id
   subnet_ids      = var.vpc_module_state == "" ? var.subnet_ids : concat(var.subnet_ids, data.terraform_remote_state.vpc[0].outputs.public_subnet_ids)
