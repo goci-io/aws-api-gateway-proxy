9aa1dee60f5579a0856a3e8c5285d4484799cda5
etwillbefine <tim.gatzemeier@gmx.de>
2019-11-02 03:54:22 +0100

    deployment cannot depend on stage

--- main.tf
+++ main.tf
@@ -88,11 +88,7 @@ resource "aws_api_gateway_stage" "stage" {
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  depends_on  = [
-    aws_api_gateway_integration.vpc, 
-    aws_api_gateway_stage.stage,
-  ]
-  
+  depends_on  = [aws_api_gateway_integration.vpc]
   rest_api_id = aws_api_gateway_rest_api.main.id
   stage_name  = var.stage
 }
